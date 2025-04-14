3149956ac70a0b401101d6e7123b415222990142
filison <65769401+filison@users.noreply.github.com>
2025-04-14 20:51:23 +0200

    home.js

--- home.js
+++ home.js
@@ -1,4 +1,12 @@
 document.getElementById("logoutBtn").addEventListener("click", () => {
   localStorage.removeItem("userLoggedIn");
   window.location.href = "login.html";
+  document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('.tip-toggle').forEach(button => {
+    button.addEventListener('click', () => {
+      const content = button.nextElementSibling;
+      content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+  });
+});
 });
